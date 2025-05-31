3b6c9926e2b397bcc485e1c5a4d15cd8f9854075
Yavor Nikiforov <yavornikiforov52@gmail.com>
2025-05-31 16:23:46 +0300

    FIxed word correctness check

--- project2/Interface.cs
+++ project2/Interface.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace project2
+﻿namespace project2
 {
     class View
     {
@@ -17,26 +11,44 @@ namespace project2
             Console.WriteLine("          W O R D L E"); // 10 space-a
             Console.ResetColor();
         }
-        public string Try(Gameplay gm)
+        public string Try(Gameplay gm, string[] listOfWords)
         {
             Console.Write("Make your guess: ");
-            string guess = Console.ReadLine().ToLower();
-            int a = (int)guess[0]; // ASCII a-97 z-122
-            int b = (int)guess[1];
-            int c = (int)guess[2];
-            int d = (int)guess[3];
-            int e = (int)guess[4];
-            while (guess.Length != 5 || a > 122 || a < 97 || b > 122 || b < 97 || c > 122 || c < 97 || d > 122 || d < 97 || e > 122 || e < 97)
+
+            while (true)
             {
-                Console.Write("  Your guess must contain only 5 LETTERS\nMake a valid guess: ");
-                guess = Console.ReadLine().ToLower();
-                a = (int)guess[0]; // ASCII a-97 z-122
-                b = (int)guess[1];
-                c = (int)guess[2];
-                d = (int)guess[3];
-                e = (int)guess[4];
+                bool error = false;
+                string guess = Console.ReadLine().ToLower();
+
+                if (guess.Length != 5)
+                {
+                    Console.Write("  Your guess must contain only 5 LETTERS\nMake a valid guess: ");
+                    continue;
+                }
+
+                foreach (char c in guess)
+                {
+                    if (c > 122 || c < 97)
+                    {
+                        Console.Write("  Your guess must contain only 5 LETTERS\nMake a valid guess: ");
+                        error = true;
+                        break;
+                    }
+                }
+
+                if (!listOfWords.Contains(guess))
+                {
+                    Console.WriteLine("  Not a real word\nMake a valid guess: ");
+                    continue;
+                }
+
+                if (error)
+                    continue;
+
+                return guess;
             }
-            return guess;
+
+
         }
         public void WriteWord(Gameplay gm, string guess)
         {
@@ -50,17 +62,20 @@ namespace project2
                 Console.Write($"          |{char.ToUpper(guess[0])}| ");
                 Console.ResetColor();
 
-            } else if (guess[0] == guess[1] || guess[0] == guess[2] || guess[0] == guess[3] || guess[0] == guess[4])
+            }
+            else if (guess[0] == guess[1] || guess[0] == guess[2] || guess[0] == guess[3] || guess[0] == guess[4])
             {
                 Console.Write($"          |{char.ToUpper(guess[0])}| ");
 
-            } else if (gm.Choice.Contains(guess[0]))
+            }
+            else if (gm.Choice.Contains(guess[0]))
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write($"          |{char.ToUpper(guess[0])}| ");
                 Console.ResetColor();
 
-            }else
+            }
+            else
             {
                 Console.Write($"          |{char.ToUpper(guess[0])}| ");
             }
--- project2/Program.cs
+++ project2/Program.cs
@@ -9,27 +9,29 @@
             //List s vsichki dumi s 5 bukvi
             // https://github.com/cheaderthecoder/5-Letter-words/blob/main/words.txt
 
-            string location = "C:\\Users\\User\\source\\repos\\project2\\project2\\bin\\words.txt";
-            StreamReader sr = new StreamReader(location); //which pupal
-            string txt = sr.ReadToEnd();
-            string[] words = txt.Split('\n');
-            int i = 0;
-            words[5757] = "chime";
-            foreach (string word in words)
+            string location = "words.txt"; // smenih streamReader, zashtoto neshto ne stavashe da razpoznava dali dumata q ima i chatgpt mi predloji tova da izpolzvam
+            string[] words = File.ReadAllLines(location);
+
+            for (int i = 0; i < words.Length; i++)
             {
-                    words[i] = word.Substring(0, 5);
+                words[i] = words[i].Trim().Substring(0, 5);
             }
+
+            if (words.Length > 5756)
+            {
+                words[5756] = "chime";
+            }
+
             View view = new View();
             view.Wordle();
-            
+
             Gameplay gm = new Gameplay();
             gm.ChooseWord(words);
-            view.WriteWord(gm, view.Try(gm));
-            view.WriteWord(gm, view.Try(gm));
-            view.WriteWord(gm, view.Try(gm));
-            view.WriteWord(gm, view.Try(gm));
-            view.WriteWord(gm, view.Try(gm));
-            view.WriteWord(gm, view.Try(gm));
+            for (int j = 0; j < 6; j++)
+            {
+                view.WriteWord(gm, view.Try(gm, words));
+            }
+
             Console.WriteLine(gm.Choice);
 
 
