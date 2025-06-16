abc6ce0cc003c9a9274d2b67c6786fd899d6deaf
Yavor Nikiforov <yavornikiforov52@gmail.com>
2025-06-16 18:29:53 +0300

    keyboard and other stuff

--- project2/Interface.cs
+++ project2/Interface.cs
@@ -8,181 +8,126 @@
         public void Wordle()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("          W O R D L E"); // 10 space-a
+            Console.WriteLine("\n          W O R D L E\n");
             Console.ResetColor();
         }
-        public string Try(Gameplay gm, string[] listOfWords)
-        {
-            Console.Write("Make your guess: ");
 
+        public string Try(Gameplay gm, string[] listOfWords, List<string> pastGuesses)
+        {
             while (true)
             {
-                bool error = false;
+                Console.Clear();
+                Wordle();
+                WriteBoard(gm, pastGuesses);
+                DisplayKeyboard(gm);
+
+                Console.Write("Make your guess: ");
                 string guess = Console.ReadLine().ToLower();
 
                 if (guess.Length != 5)
                 {
-                    Console.Write("  Your guess must contain only 5 LETTERS\nMake a valid guess: ");
+                    Console.WriteLine("  Your guess must contain exactly 5 LETTERS.");
+                    Console.WriteLine("  Press ENTER to try again...");
+                    Console.ReadLine();
                     continue;
                 }
 
                 foreach (char c in guess)
                 {
-                    if (c > 122 || c < 97)
+                    if (c < 'a' || c > 'z')
                     {
-                        Console.Write("  Your guess must contain only 5 LETTERS\nMake a valid guess: ");
-                        error = true;
-                        break;
+                        Console.WriteLine("  Only lowercase English letters are allowed.");
+                        Console.WriteLine("  Press ENTER to try again...");
+                        Console.ReadLine();
+                        continue;
                     }
                 }
 
                 if (!listOfWords.Contains(guess))
                 {
-                    Console.WriteLine("  Not a real word\nMake a valid guess: ");
+                    Console.WriteLine("  Not a real word.");
+                    Console.WriteLine("  Press ENTER to try again...");
+                    Console.ReadLine();
                     continue;
                 }
 
-                if (error)
-                    continue;
-
                 return guess;
             }
-
-
         }
-        public void WriteWord(Gameplay gm, string guess)
+
+
+        public void WriteBoard(Gameplay gm, List<string> guesses)
         {
-            //gm.Comparer(guess, gm.Choice);
-
-            //purva bukva
-            #region
-            if (guess[0] == gm.Choice[0])
+            for (int row = 0; row < 6; row++)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"          |{char.ToUpper(guess[0])}| ");
-                Console.ResetColor();
+                Console.Write("          ");
+                if (row < guesses.Count)
+                {
+                    string guess = guesses[row];
+                    for (int i = 0; i < 5; i++)
+                    {
+                        char letter = guess[i];
+                        if (gm.Choice[i] == letter)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else if (gm.Choice.Contains(letter))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                        }
 
+                        Console.Write($"|{char.ToUpper(letter)}|");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.Write("| |");
+                    }
+                }
+                Console.WriteLine();
             }
-            else if (guess[0] == guess[1] || guess[0] == guess[2] || guess[0] == guess[3] || guess[0] == guess[4])
-            {
-                Console.Write($"          |{char.ToUpper(guess[0])}| ");
-
-            }
-            else if (gm.Choice.Contains(guess[0]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"          |{char.ToUpper(guess[0])}| ");
-                Console.ResetColor();
-
-            }
-            else
-            {
-                Console.Write($"          |{char.ToUpper(guess[0])}| ");
-            }
-            #endregion
-
-            // vtora bukva
-            #region
-            if (guess[1] == gm.Choice[1])
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"|{char.ToUpper(guess[1])}| ");
-                Console.ResetColor();
-
-            }
-            else if (guess[1] == guess[2] || guess[1] == guess[3] || guess[1] == guess[4])
-            {
-                Console.Write($"|{char.ToUpper(guess[1])}| ");
-
-            }
-            else if (gm.Choice.Contains(guess[1]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"|{char.ToUpper(guess[1])}| ");
-                Console.ResetColor();
-
-            }
-            else
-            {
-                Console.Write($"|{char.ToUpper(guess[1])}| ");
-            }
-            #endregion
-
-            // treta bukva
-            #region
-            if (guess[2] == gm.Choice[2])
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"|{char.ToUpper(guess[2])}| ");
-                Console.ResetColor();
-
-            }
-            else if (guess[2] == guess[3] || guess[2] == guess[4])
-            {
-                Console.Write($"|{char.ToUpper(guess[2])}| ");
-
-            }
-            else if (gm.Choice.Contains(guess[2]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"|{char.ToUpper(guess[2])}| ");
-                Console.ResetColor();
-
-            }
-            else
-            {
-                Console.Write($"|{char.ToUpper(guess[2])}| ");
-            }
-            #endregion
-
-            // chetvurta bukva
-            #region
-            if (guess[3] == gm.Choice[3])
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"|{char.ToUpper(guess[3])}| ");
-                Console.ResetColor();
-
-            }
-            else if (guess[3] == guess[4])
-            {
-                Console.Write($"|{char.ToUpper(guess[3])}| ");
-
-            }
-            else if (gm.Choice.Contains(guess[3]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"|{char.ToUpper(guess[3])}| ");
-                Console.ResetColor();
-
-            }
-            else
-            {
-                Console.Write($"|{char.ToUpper(guess[3])}| ");
-            }
-            #endregion
-
-            // peta bukva
-            #region
-            if (guess[4] == gm.Choice[4])
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"|{char.ToUpper(guess[4])}| ");
-                Console.ResetColor();
-
-            }
-            else if (gm.Choice.Contains(guess[4]))
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"|{char.ToUpper(guess[4])}| ");
-                Console.ResetColor();
-
-            }
-            else
-            {
-                Console.Write($"|{char.ToUpper(guess[4])}| ");
-            }
-            #endregion
+            Console.WriteLine();
         }
+
+        public void DisplayKeyboard(Gameplay gm)
+        {
+            string qwerty = "qwertyuiopasdfghjklzxcvbnm";
+
+            Console.WriteLine("\n");
+            foreach (char c in qwerty)
+            {
+                if (gm.GreenLetters.Contains(c))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (gm.YellowLetters.Contains(c))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (gm.GrayLetters.Contains(c))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+
+                Console.Write($" {char.ToUpper(c)} ");
+                Console.ResetColor();
+
+                if (c == 'p' || c == 'l' || c == 'm')
+                    Console.WriteLine();
+            }
+            Console.WriteLine("\n");
+        }
+
     }
 }
